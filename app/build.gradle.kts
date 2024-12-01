da4b9d012f1c90e8fe01714b939e4929fc58856a
Maksim Medvedev <maksim.9678@mail.ru>
2024-12-01 22:39:48 +0300

    RA-02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.androidsprint01"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.androidsprint01"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 28
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
