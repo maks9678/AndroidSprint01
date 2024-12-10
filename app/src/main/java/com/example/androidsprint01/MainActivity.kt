e8fddaa310969d4b3da428ebc3aab121d7ba30ba
Maksim Medvedev <maksim.9678@mail.ru>
2024-12-10 21:53:26 +0300

    RA-03 ViewBinding

--- app/src/main/java/com/example/androidsprint01/MainActivity.kt
+++ app/src/main/java/com/example/androidsprint01/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidsprint01
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.androidsprint01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +13,17 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setStatusAndNavigationBarColor()
     }
-    private fun setStatusAndNavigationBarColor() {
-        val customColor = getColor(R.color.fon_categories)
-        window.statusBarColor = customColor
-        window.navigationBarColor = customColor
+
+    private fun setStatusAndNavigationBarColor(
+        fonStatusBar: Int = R.color.fon_status_bar,
+        fonNavigationBar: Int = R.color.fon_navigation_bar
+    ) {
+        val statusBarColor = ContextCompat.getColor(this, fonStatusBar)
+        val navigationBarColor = ContextCompat.getColor(this, fonNavigationBar)
+
+        window.statusBarColor = statusBarColor
+        window.navigationBarColor = navigationBarColor
     }
 }
