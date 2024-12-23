e3de8ce79e1e12033193764952f166896aafa5cc
maksim <maksim.9678@mail.ru>
2024-12-23 22:32:21 +0300

    RA-07 layout categories

--- app/src/main/java/com/example/androidsprint01/MainActivity.kt
+++ app/src/main/java/com/example/androidsprint01/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.androidsprint01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,20 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<CategoriesListFragment>(R.id.main_container)
+            }
+        }
+        binding.buttonFavorites.setOnClickListener {
+            supportFragmentManager.commit {
+                replace<FavoritesFragment>(R.id.main_container)
+                setReorderingAllowed(true)
+                addToBackStack(null)
+            }
+        }
+        binding.buttonCategories.setOnClickListener {
+            supportFragmentManager.commit {
+                replace<CategoriesListFragment>(R.id.main_container)
+                setReorderingAllowed(true)
+                addToBackStack(null)
             }
         }
     }
