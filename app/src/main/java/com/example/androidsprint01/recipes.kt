3cb72d1005859a99b35313ad5c3f41f28774d6d7
maksim <maksim.9678@mail.ru>
2025-01-17 12:39:51 +0300

    RA-06 data classes, STUB

--- app/src/main/java/com/example/androidsprint01/recipes.kt
+++ app/src/main/java/com/example/androidsprint01/recipes.kt
@@ -1,26 +1,5 @@
 package com.example.androidsprint01
 
-data class Recipes(
-    val id: Int,
-    val title: String,
-    val ingredients: Ingredient,
-    val method: List<String>,
-    val imageUrl: String
-)
-
-data class Categories(
-    val id: Int,
-    val title: String,
-    val description: String,
-    val imageUrl: String
-)
-
-data class Ingredient(
-    val quantity: Float,
-    val unitOfMeasure: String,
-    val description: String
-)
-
 object STUB {
     private val categories: List<Categories> = listOf(
         Categories(0, "Бургеры", "Рецепты всех популярных видов бургеров", "burger.png"),
