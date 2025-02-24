3465bba951ac3caa0360ce38d27faf620dc8b305
maks9678 <115086005+maks9678@users.noreply.github.com>
2025-02-25 00:36:49 +0300

    RA-18 layout favorites

--- app/src/main/java/com/example/androidsprint01/FavoritesFragment.kt
+++ app/src/main/java/com/example/androidsprint01/FavoritesFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.androidsprint01.BackendSingleton.getRecipesByIds
 import com.example.androidsprint01.databinding.FragmentFavoritesBinding
+import com.example.androidsprint01.recipesList.RecipesListAdapter
 
 class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
     private var _binding: FragmentFavoritesBinding? = null
     private val binding get() = _binding?: throw  IllegalStateException("Binding accessed before initialized")
+    var recycler: RecipesListAdapter? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,5 +20,14 @@ class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
     ): View {
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun getFavorites():Set<Int> {
+
+    }
+
+    fun setRecycler() {
+        val favoritesRecipe = getFavorites()
+        recycler = RecipesListAdapter(getRecipesByIds(favoritesRecipe))
     }
 }
