7a742b2d8bb0a1c5eb1fa06f62fce9696f84c153
maks9678 <115086005+maks9678@users.noreply.github.com>
2025-02-27 00:16:44 +0300

    RA-22 state

--- app/src/main/java/com/example/androidsprint01/ui/recipes/recipe/ViewModelRecipe.kt
+++ app/src/main/java/com/example/androidsprint01/ui/recipes/recipe/ViewModelRecipe.kt
@@ -1,9 +1,20 @@
 package com.example.androidsprint01.ui.recipes.recipe
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.androidsprint01.model.Recipe
+import java.util.Observer
 
 class ViewModelRecipe() : ViewModel() {
+    private var _state: MutableLiveData<String>? =null
+
+ val state
+    get() = _state ?: throw IllegalStateException("LiveData accessed before initialized")
+
+    override fun onCleared() {
+
+    }
 
     data class RecipeState(
         val recipe: Recipe? = null,
