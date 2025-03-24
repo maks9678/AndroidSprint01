84f449d0ff9f67d6d44e328c01f9952834adbb73
maks9678 <115086005+maks9678@users.noreply.github.com>
2025-03-24 21:35:54 +0300

    RA-32 URLConnection

--- app/src/main/java/com/example/androidsprint01/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidsprint01/ui/MainActivity.kt
@@ -9,6 +9,9 @@ import java.net.HttpURLConnection
 import java.net.URL
 import android.util.Log
 import com.example.androidsprint01.model.Categories
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import kotlin.concurrent.thread
@@ -29,33 +32,24 @@ class MainActivity : AppCompatActivity() {
         binding.buttonCategories.setOnClickListener {
             findNavController(R.id.nav_host_fragment).navigate(R.id.categoriesListFragment)
         }
-        val thread = Thread{
+        val thread = Thread {
             val url = URL("https://recipes.androidsprint.ru/api/category")
-            val connection = url.openConnection() as HttpURLConnection
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
             connection.connect()
 
-            val currentThread = Thread.currentThread().name
+            Log.i("!!!", "Выполняю запрос на потоке:${Thread.currentThread().name}")
+            Log.i("!!!", "responseCode:${connection.responseCode}")
+            Log.i("!!!", "responseMessage:${connection.responseMessage}")
 
-            Log.i("!!!","Выполняю запрос на потоке:${currentThread}")
-            Log.i("!!!","responseCode:${connection.responseCode}")
-            Log.i("!!!","responseMessage:${connection.responseMessage}")
-            val responseBody = connection.inputStream.bufferedReader().toString()
-            Log.i("!!!","Body:${responseBody}")
+            val responseBody = connection.inputStream.bufferedReader().readText()
+            Log.i("!!!", "Body:${responseBody}")
 
-            val categories:List<Categories> = Json.decodeFromString(responseBody)
-            categories.forEach{
-
-                Log.i("!!!","Body:${it}")
+            val categories: List<Categories> = Json.decodeFromString(responseBody)
+            categories.forEach {
+                Log.i("!!!", "Body:${it.title}")
             }
-
         }
         thread.start()
-        val currentThread1 = Thread.currentThread().name
-        Log.i("!!!","Метод onCreate() выполняется на потоке:${currentThread1}")
-
-        thread.start()
-
-        // Логируем текущий поток
         Log.i("!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
     }
 
